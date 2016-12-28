06cadff435c78ea0fddb86cb813fe70a5ec501ce
KuoChiaCheng <kuochiachen@gmail.com>
2016-12-28 12:06:26 +0800

    20161228 leaflet

--- Expert_leaflet/tab2.R
+++ Expert_leaflet/tab2.R
@@ -1,15 +1,20 @@
 # tab 2 行政區別#
-#' 行政區
-Showregion <- eventReactive(input$region, {
-  which(stores$region == input$region)
+#' 臺北市行政區
+Showregion <- eventReactive(input$TaipeiRegion, {
+  which(stores$region == input$TaipeiRegion)
 })
-#' 行政區
-observeEvent(input$region, {
+#' 新北市行政區
+Showregion <- eventReactive(input$NewtaipeiRegion, {
+  which(stores$region == input$NewtaipeiRegion)
+})
+
+#' 臺北市行政區
+observeEvent(input$TaipeiRegion, {
   Id2 = Showregion()
-  m2 <- leafletProxy('mymap2',session) %>% clearMarkers()
-  for( i in 1:length(input$region) )
+  m2 <- leafletProxy('mymap2',session)
+  for( i in 1:length(input$TaipeiRegion) )
   {
-    subid = which(stores$region == input$region[i])
+    subid = which(stores$region == input$TaipeiRegion[i])
     iconF = stores$region[subid[1]]
     iconF = paste('i_view.png', sep='')
     
@@ -21,6 +26,26 @@ observeEvent(input$region, {
     lng.path = stores$lan[subid]
     lat.path = stores$lat[subid]
     m2 <- addMarkers(m2, lng=lng.path,lat=lat.path, icon=LeafIcon)
+  }
+})
+#' 新北市行政區
+observeEvent(input$NewtaipeiRegion, {
+  Id3 = Showregion()
+  m3 <- leafletProxy('mymap2',session)
+  for( i in 1:length(input$NewtaipeiRegion) )
+  {
+    subid = which(stores$region == input$NewtaipeiRegion[i])
+    iconF = stores$region[subid[1]]
+    iconF = paste('i_view.png', sep='')
+    
+    LeafIcon <- makeIcon(
+      iconUrl = iconF,
+      iconWidth = 18, iconHeight = 18,
+      iconAnchorX = 18, iconAnchorY = 18)
+    
+    lng.path = stores$lan[subid]
+    lat.path = stores$lat[subid]
+    m3 <- addMarkers(m3, lng=lng.path,lat=lat.path, icon=LeafIcon)
   }
 })
 
